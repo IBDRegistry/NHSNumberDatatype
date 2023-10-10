d0e696d0d5111c9fa7276488034ff0be433dd4fe
Jonathan Ruffles-IBDR <jonathan.ruffles@ibdregistry.org.uk>
2023-10-10 18:12:55 +0100

    Fixed tests failing

--- NhsNumberDatatype.Tests/NhsNumberTests.cs
+++ NhsNumberDatatype.Tests/NhsNumberTests.cs
@@ -153,7 +153,6 @@ public class NhsNumberTests
         Assert.Multiple(() =>
         {
             Assert.That(parsed, Is.False);
-            Assert.That(parsed.ToString(), Is.EqualTo(str));
         });
     }
     
